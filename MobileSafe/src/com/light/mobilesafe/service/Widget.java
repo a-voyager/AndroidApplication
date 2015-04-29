7dae5189be1d55a9b75dfa16680ba52e021d2115
wuhaojie <w19961009@126.com>
2015-04-29 22:09:03 +0800

    完成桌面进程清理小空间的界面部分.

---
+++ MobileSafe/src/com/light/mobilesafe/service/Widget.java
@@ -0,0 +1,11 @@
+package com.light.mobilesafe.service;
+
+import android.appwidget.AppWidgetProvider;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Widget extends AppWidgetProvider {
+
+
+}
